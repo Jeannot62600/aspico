e7a80b562052ec338d9a58c6e8247cc4fa780821
Jean-Philippe DEMORY <lapostolle.jean@gmail.com>
2016-12-02 02:21:43 +0100

    alex

---
+++ annonces.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Nouvelles Actu</title>
+        <meta charset="utf-8"/>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/cssnews.css" rel="stylesheet">
+    </head>
+    <body>
+        <header id="base_header">
+        <?php include('header.php');?>
+	        <div class="container">
+		        <div class="row">
+		        	<div class="col-xs-12"><h1>Je veux aider</h1></div>
+				</div>
+			</div>
+        </header>
+        <main>
+        	<article class="article">
+	    	<header>
+	        	<div class="container">
+		        	<div class="row">
+						<div class="col-xs-12 text-center"> 
+							<a href="depot.php"><button id="button1" type="button" class="btn btn-primary btn-lg btn-block">Déposer une annonce</button></a>
+						</div>
+						<div class="col-xs-12 text-center"> 
+							<a href="volontaires.php"><button id="button2" type="button" class="btn btn-primary btn-lg btn-block">Proposer son aide</button></a>
+						</div>
+						<div class="col-xs-12 text-center"> 
+							<a href="Tchat.php"><button id="button3" type="button" class="btn btn-primary btn-lg btn-block">Tchat</button></a>
+						</div>
+					</div>
+				</div>
+	    	</header>
+	    	<section class="sectionannonces">
+	    	<div class="container">
+		        <div class="row">
+					<div class="col-xs-12">
+	        		</div>
+				</div>
+			</div>
+			<footer>
+			</footer>
+	    	</section>
+	    	</article>
+	    	
+	    </main>
+	    <footer></footer>
+
+    </body>
+</html>
+
+
